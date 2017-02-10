d9cf75518c7a05226e143c0e345479fd6c6d5404
scrichards92 <richards.sierra.c@gmail.com>
2017-02-09 22:29:33 -0600

    Update tail_insertion.cpp

--- assignments/tail_insertion.cpp
+++ assignments/tail_insertion.cpp
@@ -1,1 +1,83 @@
+#include <iostream>
 
+struct Node {
+	int Data;
+	Node *Next;
+};
+
+using namespace std;
+
+class List {
+private:
+	Node *Head;
+	Node *Tail;
+public:
+	List() {
+		Head = NULL;
+	}
+
+	void FrontSert(int x) {
+		Node *Temp = new Node;
+		Temp->Data = x;
+		Temp->Next = NULL;
+
+		if (Head) {
+			Temp->Next = Head;
+		}
+
+		if (!Head)
+		{
+			Tail = Temp;
+		}
+
+		Head = Temp;
+	}
+
+
+	void EndSert(int x)
+	{
+		Node *Temp = new Node;
+		Temp->Data = x;
+		Temp->Next = NULL;
+
+		if (!Head)
+		{
+			Head = Temp;
+			Tail = Head;
+		}
+		else
+		{
+			Tail->Next = Temp;
+			Tail = Temp;
+		}
+	}
+
+	void PrintList() {
+		if (!Head) {
+			cout << "Empty" << endl;
+			return;
+		}
+		else {
+			Node *Temp = Head;
+			while (Temp != NULL) {
+				cout << Temp->Data << "->";
+				Temp = Temp->Next;
+			}
+		}
+	}
+
+};
+
+
+int main() {
+	srand(97097807);
+	List L;
+	for (int i = 0; i<10; i++) {
+		L.FrontSert(rand() % 100);
+	}
+	L.EndSert(rand() % 100);
+	L.PrintList();
+
+
+	return 0;
+}
